26f8e93897f69a8d0a0ba4efb0a24d9b9206658d
craigsuckling <156681544+craigsuckling@users.noreply.github.com>
2025-01-31 12:04:17 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import CountrySelect from '../components/CountrySelect';
@@ -26,7 +25,7 @@ export default function Home() {
     
       
         
-          European Capitals Finder
+          {'European Capitals Finder'}
         
         
         
@@ -41,4 +40,3 @@ export default function Home() {
     
   );
 }
-
